7dea17e3c683c3018d83abec59a7911d66d0deae
Iván Castelán 👾 <gho0thubun@gmail.com>
2022-07-28 20:20:02 -0500

    First commit

---
+++ hello_world.py
@@ -0,0 +1,17 @@
+print('Hello world')
+
+a = 10
+
+b = 20
+
+if (a > b):
+  print(' a es mayo a b')
+else:
+  print('b es mayor a A')
+
+
+for i in range(10):
+  print(i)
+
+
+print('Estoy en otra rama')
